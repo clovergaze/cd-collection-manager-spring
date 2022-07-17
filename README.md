7a2626f2d3a495b541cb24cbd25bd8a257103c97
Johannes Hillert <johannes.hillert@infokin.org>
2022-07-17 14:38:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CD Collection Manager (Spring)
+
+A simple example application that uses Spring technologies.
+
+## License
+
+Copyright (c) 2022 Johannes Hillert. Licensed under the MIT license, see the included LICENSE file for details.
---
+++ src/main/java/org/infokin/cdcollectionmanagerspring/CdCollectionManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.infokin.cdcollectionmanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdCollectionManagerSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CdCollectionManagerSpringApplication.class, args);
+  }
+
+}
