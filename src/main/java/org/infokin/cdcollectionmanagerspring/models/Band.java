6808cefeb657be6415b0427ceaafe92b3c630270
Johannes Hillert <johannes.hillert@infokin.org>
2022-07-17 20:39:33 +0200

    added initialization for property

--- src/main/java/org/infokin/cdcollectionmanagerspring/models/Band.java
+++ src/main/java/org/infokin/cdcollectionmanagerspring/models/Band.java
@@ -5,6 +5,7 @@ import org.infokin.cdcollectionmanagerspring.models.core.BaseEntity;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,7 +15,7 @@ public class Band extends BaseEntity {
   private String name;
 
   @OneToMany(mappedBy = "band")
-  private Set<Album> albums;
+  private Set<Album> albums = new HashSet<>();
 
   public Band() {
   }
